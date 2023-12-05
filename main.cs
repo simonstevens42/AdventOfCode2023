87439767f4789fa26fd548be4ef8dc416c0adc6b
simonstevens42 <mail.simon.stevens@proton.me>
2023-12-05 23:37:22 +0100

    Day02 PartTwo completed

--- main.cs
+++ main.cs
@@ -9,7 +9,8 @@ partial class Program
     {
         // Day01PartOne();
         // Day01PartTwo();
-        Day02PartOne();
+        // Day02PartOne();
+        Day02PartTwo();
     }
 
     // Day 01 - Part One
@@ -206,5 +207,68 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
     {
         Match match = Regex.Match(input, @"\d+\s+(\w+)");
         return match.Success ? match.Groups[1].Value : string.Empty;
+    }
+
+    // Day 02 - Part Two
+    static void Day02PartTwo()
+    {
+        string input =
+        @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        string[] games = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        int sumPowerSets = 0;
+
+        for (int i = 0; i < games.Length; i++)
+        {
+
+            games[i] = Regex.Replace(games[i], @"^Game \d+: ", string.Empty);
+
+            string[] gamesSplit = games[i].Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            foreach (string game in gamesSplit)
+            {
+                string[] cubes = game.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string cubeColor in cubes)
+                {
+                    int number = ExtractNumber(cubeColor);
+                    string color = ExtractColor(cubeColor);
+
+                    if (color == "red")
+                    {
+                        if (number >= red)
+                        {
+                            red = number;
+                        }
+                    }
+                    if (color == "green")
+                    {
+                        if (number >= green)
+                        {
+                            green = number;
+                        } 
+                    }
+                    if (color == "blue")
+                    {
+                        if (number >= blue)
+                        {
+                            blue = number;
+                        }    
+                    }
+                }
+            }
+            sumPowerSets += (red * green * blue);
+        }
+
+        Console.WriteLine(sumPowerSets);
     }
 }
